7806629b3ac2a2bec37ab9c7132881758752207c
xectrone <bhushanmalekar619@gmail.com>
2024-01-13 10:41:38 +0530

    version 6
    Migration of Home Screen

--- app/src/main/java/com/example/remup/ui/home_screen/HomeScreen.kt
+++ app/src/main/java/com/example/remup/ui/home_screen/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -19,30 +20,29 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import com.example.remup.data.view_model.NoteViewModel
 import com.example.remup.domain.navigation.Screen
+import com.example.remup.ui.home_screen.search_app_bar.NoteListItem
 import com.example.remup.ui.home_screen.search_app_bar.SearchAppBar
 import com.example.remup.ui.home_screen.search_app_bar.SearchAppBarViewModel
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
-    noteViewModel: NoteViewModel = hiltViewModel(),
+    viewModel: HomeViewModel = hiltViewModel(),
     searchAppBarViewModel: SearchAppBarViewModel =  hiltViewModel(),
     navController: NavHostController
 ){
 
-    val originalNoteList = noteViewModel.noteList().collectAsState(initial = emptyList()).value
-    var noteList by remember { mutableStateOf(originalNoteList) }
-    noteList = originalNoteList
+    viewModel.getFlashcardList()
+    val noteList by viewModel.noteList.collectAsStateWithLifecycle()
+    val selectionMode by viewModel.selectionMode
 
-
-    var selectionMode by remember { mutableStateOf(false) }
     val searchAppBarState by searchAppBarViewModel.searchAppBarState
     val searchTextState by searchAppBarViewModel.searchTextState
 
@@ -51,18 +51,14 @@ fun HomeScreen(
         topBar = {
             SearchAppBar(
                 text = searchTextState,
-                onTextChange = { it ->
+                onTextChange = {
                     searchAppBarViewModel.updateSearchTextState(it)
-                    noteList =
-                        originalNoteList.filter { it.doesMatchSearchQuery(searchTextState) } },
-                onCloseClicked = { noteList = originalNoteList },
+                    viewModel.onSearch(it) },
+                onCloseClicked = { viewModel.onSearchClose() },
                 onSearchClicked = { },
                 onSideButtonClick = { navController.navigate(Screen.ShowNote.route)}
             )
         },
-        //endregion
-
-
         //endregion
 
         //region - Floating Action Button -
@@ -92,43 +88,20 @@ fun HomeScreen(
             verticalArrangement = Arrangement.Top,
             horizontalArrangement = Arrangement.Start
         ){
-            items(noteList.size){
-                Box(
-                    modifier = modifier
-                        .padding(4.5.dp)
-                        .height(150.dp)
-                        .clip(RoundedCornerShape(17.dp))
-                        .background(MaterialTheme.colors.surface)
-                        .clickable { navController.navigate(Screen.AddEditNote.navArg(noteList[it].id)) },
-                    contentAlignment = Alignment.TopStart)
-                {
-                    Column(
-                        modifier = modifier
-                            .padding(25.dp)
-                            .fillMaxSize(),
-                        verticalArrangement = Arrangement.SpaceBetween
+            items(noteList){
+                NoteListItem(
+                    item = it,
+                    onClick = {
+                        if (selectionMode)
+                            viewModel.onItemClick(it)
+                        else {
+//                            searchAppBarViewModel.updateSearchAppBarState(SearchAppBarState.CLOSED)
+                            navController.navigate(route = Screen.AddEditNote.navArg(it.note.id))
+                        }
+                    },
+                    onLongClick = { viewModel.onItemLongClick(it) }
+                )
 
-                    )
-                    {
-                        Text(
-                            modifier = modifier
-                                .padding(bottom = 15.dp),
-                            text = noteList[it].data ,
-                            color = MaterialTheme.colors.primary,
-                            maxLines = 4,
-                            textAlign = TextAlign.Justify,
-                        )
-
-                        Text(
-                            fontSize = 13.sp,
-                            text = noteList[it].created.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM, FormatStyle.SHORT)),
-                            color = MaterialTheme.colors.primaryVariant,
-                            maxLines = 1,
-                            textAlign = TextAlign.Start
-                        )
-                    }
-
-                }
             }
         }
     }
